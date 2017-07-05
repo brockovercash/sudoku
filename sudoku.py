0fbfbea43aecfbb8135105221523ffc8ae4e05eb
Brock Overcash <brock.overcash@gmail.com>
2017-07-05 16:04:40 -0400

    Fix flake8 linter errors

--- sudoku.py
+++ sudoku.py
@@ -1,41 +1,44 @@
 import copy
 
+
 def printCounts(grid):
-  counts = [0] * 10
-  for i in range(9):
-    for j in range(9):
-      counts[grid[i][j]] += 1
-  for e in range(10):
-    if counts[e] == 9:
-      counts[e] = ' '
-    print(e, ': ', counts[e], sep="")
+    counts = [0] * 10
+    for i in range(9):
+        for j in range(9):
+            counts[grid[i][j]] += 1
+    for e in range(10):
+        if counts[e] == 9:
+            counts[e] = ' '
+        print(e, ': ', counts[e], sep='')
 
 
 def isRowSingular(grid, i, e):
-  count = 0
-  for j in range(9):
-    count += grid[i][j].count(e)
-  return count == 1
+    count = 0
+    for j in range(9):
+        count += grid[i][j].count(e)
+    return count == 1
 
 
 def isColSingular(grid, j, e):
-  count = 0
-  for i in range(9):
-    count += grid[i][j].count(e)
-  return count == 1
+    count = 0
+    for i in range(9):
+        count += grid[i][j].count(e)
+    return count == 1
 
 
 def isBoxSingular(grid, i, j, e):
-  count = 0
-  secTopX, secTopY = 3 *(i//3), 3 *(j//3)
-  for x in range(secTopX, secTopX+3):
-      for y in range(secTopY, secTopY+3):
-          count += grid[x][y].count(e)
-  return count == 1
+    count = 0
+    secTopX, secTopY = 3 * (i//3), 3 * (j//3)
+    for x in range(secTopX, secTopX+3):
+        for y in range(secTopY, secTopY+3):
+            count += grid[x][y].count(e)
+    return count == 1
 
 
 def hasSingular(grid, i, j):
-  return any([isRowSingular(grid,i,e) or isColSingular(grid,j,e) or isBoxSingular(grid,i,j,e) for e in grid[i][j]])
+    return any([isRowSingular(grid, i, e) or
+                isColSingular(grid, j, e) or
+                isBoxSingular(grid, i, j, e) for e in grid[i][j]])
 
 
 def isValid(grid, i, j, e):
@@ -43,8 +46,9 @@ def isValid(grid, i, j, e):
     if rowOk:
         columnOk = all([e != grid[x][j] for x in range(9)])
         if columnOk:
-            # finding the top left x,y co-ordinates of the section containing the i,j cell
-            secTopX, secTopY = 3 *(i//3), 3 *(j//3)
+            # Finding the top left x,y co-ordinates of the section
+            # containing the i,j cell
+            secTopX, secTopY = 3 * (i//3), 3 * (j//3)
             for x in range(secTopX, secTopX+3):
                 for y in range(secTopY, secTopY+3):
                     if grid[x][y] == e:
@@ -52,52 +56,56 @@ def isValid(grid, i, j, e):
             return True
     return False
 
+
 def solve(grid):
-  output = copy.deepcopy(grid)
-  for i in range(0 , 9):
-    for j in range(0, 9):
-      if grid[i][j] != 0:
-        output[i][j] = []
-      else:
-        valid = []
-        for e in range(1,10):
-          if isValid(grid,i,j,e):
-            valid.append(e)
-        output[i][j] = valid
-  return output
+    output = copy.deepcopy(grid)
+    for i in range(0, 9):
+        for j in range(0, 9):
+            if grid[i][j] != 0:
+                output[i][j] = []
+            else:
+                valid = []
+                for e in range(1, 10):
+                    if isValid(grid, i, j, e):
+                        valid.append(e)
+                output[i][j] = valid
+    return output
+
 
 def printBigHint(grid):
-  for i in range(0, 9):
-    if i != 0 and i%3 == 0:
-      print(150 * '_')
-    for j in range(0, 9):
-      print(grid[i][j], ((14 - len(str(grid[i][j]))) * ' '), end="")
-      if j != 8 and (j + 1)%3 == 0:
-        print('|    ', end="")
-    print('')
+    for i in range(0, 9):
+        if i != 0 and i % 3 == 0:
+            print(150 * '_')
+        for j in range(0, 9):
+            print(grid[i][j], ((14 - len(str(grid[i][j]))) * ' '), end='')
+            if j != 8 and (j + 1) % 3 == 0:
+                print('|    ', end='')
+        print('')
+
 
 def printHint(grid):
-  for i in range(0, 9):
-    if i != 0 and i%3 == 0:
-      print((6 * '_') + '|' + (7 * '_') + '|' + (6 * '_'))
-    for j in range(0, 9):
-      print(len(grid[i][j]), end=" ")
-      if j != 8 and (j + 1)%3 == 0:
-        print('| ', end="")
-    print('')
+    for i in range(0, 9):
+        if i != 0 and i % 3 == 0:
+            print((6 * '_') + '|' + (7 * '_') + '|' + (6 * '_'))
+        for j in range(0, 9):
+            print(len(grid[i][j]), end=' ')
+            if j != 8 and (j + 1) % 3 == 0:
+                print('| ', end='')
+        print('')
+
 
 def printSingles(grid):
-  for i in range(0, 9):
-    if i != 0 and i%3 == 0:
-      print((6 * '_') + '|' + (7 * '_') + '|' + (6 * '_'))
-    for j in range(0, 9):
-      if hasSingular(grid, i, j):
-        print(1, end=" ")
-      else:
-        print(0, end=" ")
-      if j != 8 and (j + 1)%3 == 0:
-        print('| ', end="")
-    print('')
+    for i in range(0, 9):
+        if i != 0 and i % 3 == 0:
+            print((6 * '_') + '|' + (7 * '_') + '|' + (6 * '_'))
+        for j in range(0, 9):
+            if hasSingular(grid, i, j):
+                print(1, end=' ')
+            else:
+                print(0, end=' ')
+            if j != 8 and (j + 1) % 3 == 0:
+                print('| ', end='')
+        print('')
 
 # 'Very difficult' puzzle generated on 7 Sudoku
 # http://www.7sudoku.com/view-puzzle?date=20170701
